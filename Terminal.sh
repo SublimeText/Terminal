89e63ca134e4cdd2702dab01fec41886942da285
Koen Lageveen <koen.lageveen@me.com>
2023-06-01 11:15:57 +0200

    mac: fix terminal opening 2 windows if not running
    if the terminal app is not running activation opens a window
    and then the current path is opened in yet another window
    
    the < 10.7.0 version check is not explained, there is no git history
    on MacOS 13.4 the applied change works as intended, and the terminal
    app has not seen any changes since 10.14 or so (10.7 is from 2011)

--- Terminal.sh
+++ Terminal.sh
@@ -2,19 +2,16 @@
 
 CD_CMD="cd "\\\"$(pwd)\\\"" && clear"
 if echo "$SHELL" | grep -E "/fish$" &> /dev/null; then
-  CD_CMD="cd "\\\"$(pwd)\\\""; and clear"
+	CD_CMD="cd "\\\"$(pwd)\\\""; and clear"
 fi
-VERSION=$(sw_vers -productVersion)
-if (( $(expr $VERSION '<' 10.7.0) )); then
-	IN_WINDOW="in window 1"
-fi
+
 osascript<<END
 try
 	tell application "System Events"
 		if (count(processes whose name is "Terminal")) is 0 then
 			tell application "Terminal"
 				activate
-				do script "$CD_CMD" $IN_WINDOW
+				do script "$CD_CMD" in window 1
 			end tell
 		else
 			tell application "Terminal"

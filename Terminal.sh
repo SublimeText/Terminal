f26edcd5045ba1b76e1451f49c6d81843c3459b7
Koen Lageveen <koen.lageveen@me.com>
2023-06-02 15:41:35 +0200

    also un-miniaturize terminal windows

--- Terminal.sh
+++ Terminal.sh
@@ -11,6 +11,7 @@ try
 		if (count(processes whose name is "Terminal")) is 0 then
 			tell application "Terminal"
 				activate
+				set miniaturized of window 1 to false
 				do script "$CD_CMD" in window 1
 			end tell
 		else

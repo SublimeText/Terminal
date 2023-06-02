60cee764847cdfe1857a603e4db1ffd1a93e2d8b
Koen Lageveen <koen.lageveen@me.com>
2023-06-02 15:41:45 +0200

    make re-use more reliable

--- TerminalReuse.sh
+++ TerminalReuse.sh
@@ -11,7 +11,15 @@ try
 		tell application "Terminal"
 			activate
 		end tell
+
+		if not (exists (window 1 of process "Terminal")) then
+			tell application "Terminal"
+				do script "" -- opens a new window
+			end tell
+		end if
+
 		tell application "Terminal"
+			set miniaturized of window 1 to false
 			do script "$CD_CMD" in window 1
 		end tell
 	end tell
